7d10a51d2ac70383a91ca94bbc82d877629272fd
Elliott <elliottbrunet@gmail.com>
2022-07-28 17:14:20 +0200

    add deployment scripts

---
+++ scripts/test.ts
@@ -0,0 +1,32 @@
+import { ethers } from 'hardhat'
+import { PrismMinting, PrismProjects } from '../typechain'
+import { PrismToken } from '../typechain'
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+async function main() {
+
+    let contractProjects: PrismProjects
+    let contractTokens: PrismToken
+    let contractMinting: PrismMinting
+
+    const [deployer] = await ethers.getSigners();
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const contents = readFileSync(join(__dirname, './address/contractAddresses.json'), 'utf-8');
+    console.log(JSON.parse(contents));
+    const contractAddresses = JSON.parse(contents)
+
+    // contractProjects = await ethers.getContractAt("PrismProjects", contractAddresses.PrismProjects, deployer) as PrismProjects
+    // contractTokens = await ethers.getContractAt("PrismToken", contractAddresses.PrismTokens, deployer) as PrismToken
+    contractMinting = await ethers.getContractAt("PrismMinting", contractAddresses.PrismMinting, deployer) as PrismMinting
+
+    // await contractMinting.setProjectIdToBundles(1, [[1, 2, 3], [2, 2, 3], [3, 3, 3, 3]], { gasLimit: 10000000 })
+
+    await contractMinting.mintRandomBundle(1, { gasLimit: 10000000 })
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
